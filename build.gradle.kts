f4e34027a1e7fe20b2decf1d96f01140cedac3b3
Geir André Lund <geir.andre.lund@nav.no>
2021-06-24 14:46:53 +0200

    Oppdatert 3.parts biblioteker - Kotlin til 1.5.10 - Gradle til 7.1 - Jackson til 2.12.3 - Kafka til 2.8.1 - prometheus client til 0.11.0* +++
    * Prometheus bruker nå OpenMetrics datamodel
    With this release the client_java simpleclient switches to the OpenMetrics data model and adds support for various new OpenMetrics-related features. This should be largely seamless, however any counters which lack a _total suffix on their sample will now have it added. If you'd prefer to make that change more gradually, you should change your metric names before upgrading to this version.
    ref https://github.com/prometheus/client_java/releases/tag/parent-0.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ apply {
 
 repositories {
     jcenter()
-    maven(url = "http://packages.confluent.io/maven/")
+    maven(url = "https://packages.confluent.io/maven/")
     maven("https://jitpack.io")
 }
 
@@ -77,9 +77,6 @@ tasks.named("shadowJar") {
     dependsOn("test")
 }
 
-tasks.named("jar") {
-    dependsOn("test")
-}
 tasks.named("compileKotlin") {
     dependsOn("spotlessCheck")
 }
