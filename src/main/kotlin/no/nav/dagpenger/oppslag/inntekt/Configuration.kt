42d8d1171bb752d64ad130a45e48910f82fc860d
Giao The Cung <giaothe@gmail.com>
2022-03-15 13:59:34 +0100

    Revert "lage httpklient som støtter proxy"
    This reverts commit 3da00a71ba684b31e9f3dbc566f472fa8c9c307e.

--- src/main/kotlin/no/nav/dagpenger/oppslag/inntekt/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/oppslag/inntekt/Configuration.kt
@@ -1,18 +1,11 @@
 package no.nav.dagpenger.oppslag.inntekt
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.natpryce.konfig.ConfigurationMap
 import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
 import com.natpryce.konfig.EnvironmentVariables
 import com.natpryce.konfig.Key
 import com.natpryce.konfig.overriding
 import com.natpryce.konfig.stringType
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.ProxyBuilder
-import io.ktor.client.engine.http
-import io.ktor.client.features.json.JacksonSerializer
-import io.ktor.client.features.json.JsonFeature
 import mu.KotlinLogging
 import no.nav.dagpenger.oauth2.CachedOauth2Client
 import no.nav.dagpenger.oauth2.OAuth2Config
@@ -51,19 +44,6 @@ internal object Configuration {
         return CachedOauth2Client(
             tokenEndpointUrl = azureAd.tokenEndpointUrl,
             authType = azureAd.clientSecret(),
-            httpClient = HttpClient() {
-                install(JsonFeature) {
-                    serializer = JacksonSerializer {
-                        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                        setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                    }
-                }
-                engine {
-                    System.getenv("HTTP_PROXY")?.let {
-                        this.proxy = ProxyBuilder.http(it)
-                    }
-                }
-            }
         )
     }
 
